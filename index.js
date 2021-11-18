8426e43f858a1ec434bb016188e46e22f3c6196a
Naji Dia <najimdia@gmail.com>
2021-11-18 14:09:36 -0800

    project docs

---
+++ index.js
@@ -0,0 +1,44 @@
+// file system call
+const fs = require("fs");
+
+const { prompt } = require("inquirer");
+const questions = require("./prompt.js");
+
+const init = async () => {
+    const { title, github, description, installation, usage, contributors, tests, license } = await prompt(
+        questions
+    );
+    const template = `
+#  ${title}
+
+##  ${github}
+
+## Description
+    ${description}
+
+## Table of Contents (Optional)
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+
+## Installation
+    ${installation}
+    
+## Usage
+    ${usage}
+
+## Contributors
+    ${contributors}
+    
+## License
+    ${license}
+    
+## Tests
+    ${tests}
+    `
+        fs.writeFileSync("./ReadME.md", template);
+        console.info("SUCESS!");
+}
+init();
